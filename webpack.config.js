b784655681b1db400f0cf1956975a62ff81b4d41
LoganVoisin <flyflameking@gmail.com>
2023-06-05 15:03:53 -0500

    init: added necessary packages

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+const srcDir = path.resolve(__dirname, 'client', 'src', 'index.tsx');
+
+module.exports = {
+  mode: 'development',
+  entry: srcDir,
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+            },
+          },
+        },
+        {
+            test: /\.tsx?$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'ts-loader',
+            },
+        },
+        {
+            test: /\.(css|scss)$/,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+            use: ['file-loader'],
+          },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client', 'dist'),
+  },
+};
