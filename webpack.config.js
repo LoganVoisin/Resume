794ac985d4f15d2eacb31718296eb825d8eb4a8c
LoganVoisin <flyflameking@gmail.com>
2023-06-05 15:09:18 -0500

    init-fix: webpack bug fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,36 @@
 const path = require('path');
 
 const srcDir = path.resolve(__dirname, 'client', 'src', 'index.tsx');
-
 module.exports = {
   mode: 'development',
-  entry: srcDir,
+  entry: path.resolve(srcDir),
   module: {
     rules: [
+      {
+        test: /.(tsx?)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+          },
+        },
+      },
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
         use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-            },
-          },
+          loader: 'ts-loader',
         },
-        {
-            test: /\.tsx?$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'ts-loader',
-            },
-        },
-        {
-            test: /\.(css|scss)$/,
-            use: ['style-loader', 'css-loader'],
-          },
-          {
-            test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-            use: ['file-loader'],
-          },
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ['file-loader'],
+      },
     ],
   },
   resolve: {
