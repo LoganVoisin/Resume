e7d07fd9fe59c16215cd6ef7f567576a8f9462df
LoganVoisin <loganvoisin1223@gmail.com>
2023-07-21 19:33:11 -0500

    update: prep for event. Changes pending

--- client/src/Protfolio.tsx
+++ client/src/Protfolio.tsx
@@ -14,8 +14,8 @@ function Portfolio() {
                     <div className='col-2'>
                         <div className='portfolio-container'>
                             <h2 className='h2-med'>TBD</h2>
-                            <p>Simultaneously watch Youtube videos with others! Chat while you watch! Archive the parties to relive the memories!</p>
-                            <img className='portfolio-img' src={require('./imgs/watchparty.jpg').default}></img>
+                            <p>This is where you make your mark!</p>
+                            <img className='portfolio-img' src={require('./imgs/Make.jpg').default}></img>
                         </div>
                     </div>
                 </div>
